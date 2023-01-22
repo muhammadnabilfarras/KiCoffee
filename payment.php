097a84210213e4d65e133d0f9a4f98a6004c0a08
Faiky12 <faiqmudah12@gmail.com>
2023-01-22 22:20:41 +0700

    FE Payment

--- payment.php
+++ payment.php
@@ -3,7 +3,6 @@ include "db.php";
 
 session_start();
 
-
 if(isset($_POST["submit"])){
     $nama = $_POST["nama"];
     $alamat = $_POST["alamat"];
@@ -14,10 +13,10 @@ if(isset($_POST["submit"])){
 $listproduk = array();
 
 if($pengiriman == "Antar"){
-    $txt_pengiriman = "antar ke rumah";
+    $txt_pengiriman = "Antar ke Rumah";
     $ongkir = 8000;
 } else {
-    $txt_pengiriman = "ambil ke cafe";
+    $txt_pengiriman = "Ambil ke Cafe";
     $ongkir = 0;
 }
 
@@ -50,39 +49,60 @@ foreach($produkId as $data){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style.css" rel="stylesheet">
     <title>Terima Kasih!</title>
 </head>
 <body>
-        <p>Pesanan untuk:</p>
-        <h1><?php echo $nama ?></h1>
-        <p>Di <?php echo $alamat ?></p>
-        <p>Metode pengiriman yang diinginkan adalah <?php echo $txt_pengiriman; ?></p>
-        <p>Nomor yang dapat dihubungi adalah <?php echo $telp ?></p>
-        <p>Dengan daftar pesanan:</p><br>
-        <table>
-            <tr>
-                <th>Nama Produk</th>
-                <th>Harga Produk</th>
-            </tr>
-            <?php
-            foreach($listproduk as $row){
-                ?>
-                <tr>
-                    <td><?php echo $row['namaProduk']; ?></td>
-                    <td><?php echo $row['hargaProduk']; ?></td>
-                </tr>
-                <?php
-            }
-            ?>
-                <tr>
-                    <th>Total:</th>
-                    <td><?php echo array_sum($totalBiaya) + $ongkir; ?></td>
-                </tr>
-        </table><br>
-        <a href="cart.php">Oke</a>
+
+<section id=pay>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Pesanan Untuk :</h5>
+                <h1><?php echo $nama ?></h1>
+                <p>Di <strong><?php echo $alamat ?></strong></p>
+                <p>Metode pengiriman yang diinginkan adalah <strong><?php echo $txt_pengiriman; ?></strong></p>
+                <p>Nomor yang dapat dihubungi adalah <strong><?php echo $telp ?></strong></p>
+            </div>
+            </div>
+        </div>
+        
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5>Daftar Pesanan :</h5><br>
+                <table>
+                    <tr>
+                        <th>Nama Produk</th>
+                        <th>Harga Produk</th>
+                    </tr>
+                    <?php
+                    foreach($listproduk as $row){
+                        ?>
+                        <tr>
+                            <td><?php echo $row['namaProduk']; ?></td>
+                            <td><?php echo $row['hargaProduk']; ?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                        <tr>
+                            <th>Total:</th>
+                            <td><?php echo array_sum($totalBiaya) + $ongkir; ?></td>
+                        </tr>
+                </table><br>
+                <a href="cart.php" class="btn btn-primary">Selesai</a>
+            </div>
+            </div>
+        </div>
+    </div> 
+</section>
+
 </body>
 </html>
