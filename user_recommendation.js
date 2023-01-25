251814d7c72e8f23206d6faae0063153af9239d9
ClintonEastwood <115123257+ClintonEastwood@users.noreply.github.com>
2023-01-25 14:21:05 +0700

    membuat sistem rekomendasi

--- user_recommendation.js
+++ user_recommendation.js
@@ -21,102 +21,155 @@ function getpilihanbot() {
     let temp = [];
     //loop sampain pilihan habis
     for (let i = 0; i < 3; i++) {
-        let norandom = Math.floor(Math.random() * idMinuman.length);
+        let norandom = Math.floor(Math.random() * 10);
         if (temp != norandom) { 
-            
             console.log(norandom);
-            console.log(idMinuman[norandom]);
-            console.log(img[norandom]);
-            let id_minuman = idMinuman[norandom];
-            let nama_minuman = namaMinuman[norandom];
-            let harga_minuman = hargaMinuman[norandom];
-            let image = img[norandom];
-            temp = norandom;
-            delete temp[-i];
-            console.log(temp);
-            // delete idMinuman[norandom];
-            // delete namaMinuman[norandom];
-            // delete hargaMinuman[norandom];
-            // delete img[norandom];
+            if (norandom == 1) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
 
-            dataList+= 
-            // `
-            // <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            //     <div class="portfolio-wrap">
-            //     <img src="assets/img/product/coffee1.jpg" class="img-fluid" alt="">
-            //     <div class="portfolio-info">
-            //         <h4>${namaMinuman}</h4><!-- Nama Product -->
-            //         ini deskripsi
-            //         <p><i>Rp ${hargaMinuman}</i></p><!-- Harga Product -->
-            //         <div class="portfolio-links">
-            //         <a href="assets/img/product/coffee1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Espresso with latte"><i class="bx bx-plus"></i></a>
-            //         <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-            //         </div>
-            //     </div>
-            //     </div>
-            // </div>
-            // `
-            `
-            <style>
-                .col-product img {
-                    margin: 1rem;
-                    width: 80%;
-                    height: 80%;
-                    border-radius: 5px;
-                    cursor: pointer;
-                    transition: 0.3s;
-                    box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
-                }
-            
-                .col-product img:hover {
-                    opacity: 0.7;
-                }
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/coffee1.jpg">
+                    <div class="layer" id="latte_art">
+                        <h3>Latte Art Machiatto <br><br><br><br> Rp 38.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 2) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
 
-                .layer{
-                    height: 80.5%;
-                    width: 80%;
-                    position: absolute;
-                    top: 14px;
-                    left: 16px;
-                    transition: 0.5s;
-                    border-radius: 5px;
-                    cursor: pointer;
-                }
-                .layer:hover{
-                    background: rgba(0,0,0,0.7);;
-                }
-                .layer h3{
-                    width: 45%;
-                    font-weight: 450;
-                    color: #fff;
-                    font-size: 26px;
-                    bottom: 0;
-                    left: 50%;
-                    transform: translateX(-50%);
-                    position: absolute;
-                    opacity: 0;
-                    transition: 0.5s;
-                    text-align: center;
-                }
-                .layer:hover h3{
-                    bottom: 10%;
-                    opacity: 0.7;
-                }
-            </style>
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/coffee2.jpg">
+                    <div class="layer" id="espresso">
+                        <h3>Espresso <br><br><br><br> Rp 25.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 3) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
 
-            <div class="col-product">
-                <img src="${image}">
-                <div class="layer" id=${id_minuman}>
-                    <h3>${nama_minuman} <br><br><br><br> Rp ${harga_minuman}</h3>
-                </div>
-            </div>`;
-        }
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/coffee3.jpg">
+                    <div class="layer" id="kopsus">
+                        <h3>Kop-Sus <br><br><br><br> Rp 23.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 4) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/drink1.jpg">
+                    <div class="layer" id="lime_lemon">
+                        <h3>Lime Lemon <br><br><br><br> Rp 25.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 5) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/drink2.jpg">
+                    <div class="layer" id="sugar_watermelon">
+                        <h3>Sugar Watermelon <br><br><br><br> Rp 20.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 6) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product" id="col-product">
+                    <img src="./assets/img/product/drink3.jpg">
+                    <div class="layer" id="manggo_juice">
+                        <h3>Manggo Juice <br><br><br><br> Rp 28.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 7) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/food3.jpg">
+                    <div class="layer" id="strawberry">
+                        <h3>Strawberry Cheese Cake <br><br><br><br> Rp 35.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 8) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/food4.jpg">
+                    <div class="layer" id="salad">
+                        <h3>Salad <br><br><br><br> Rp 20.000</h3>
+                    </div>
+                </div>`;
+            }
+            else if (norandom == 9) {
+                temp = norandom;
+                delete temp[-i];
+                console.log(temp);
+
+                dataList+= 
+                `<div class="col-product">
+                    <img src="./assets/img/product/food5.jpg">
+                    <div class="layer" id="toast">
+                        <h3>Toast Creamy Brule with Egg <br><br><br><br> Rp 40.000</h3>
+                    </div>
+                </div>`;
+            }
+            else {
+
+            }
     }
     
     console.log(dataList);
     //dibawah bisa langsung return list of inner html
     document.querySelector('#listBeverages').innerHTML = dataList;
 }
+}
 
+// idMinuman.length
+// console.log(norandom);
+//                 console.log(idMinuman[norandom]);
+//                 console.log(norandom);
+//                 console.log(img[norandom]);
+//                 console.log(norandom);
+//                 let id_minuman = idMinuman[norandom];
+//                 let nama_minuman = namaMinuman[norandom];
+//                 const harga_minuman = hargaMinuman[norandom];
+//                 const image = img[norandom];
+//                 temp = norandom;
+//                 delete temp[-i];
+//                 console.log(temp);
 
-
+//                 dataList+= 
+//                 `<div class="col-product">
+//                     <img src="${image}">
+//                     <div class="layer" id=${id_minuman}>
+//                         <h3>${nama_minuman} <br><br><br><br> Rp ${harga_minuman}</h3>
+//                     </div>
+//                 </div>`;
